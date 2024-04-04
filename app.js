56145d1c983c6cb27f59f95240dec3c6b116052a
akhterjawad <akhterjawad566@gmail.com>
2024-04-05 02:20:11 +0500

    30%

---
+++ app.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        320: {
+            items: 2
+        },
+        600: {
+            items: 4
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
